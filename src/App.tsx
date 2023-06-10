a80f03b0db8561e237cc48a36d816c8c03194a3a
gauravkharel <kharel619@gmail.com>
2023-06-10 20:57:47 +0545

    Writing my initial code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <MainMenu />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          Icon
+          <h3>Your Library</h3>
+        </div>
+        <div>
+          Icon
+          Icon
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div aria-describedby="search by">
+        <ul>
+          <li>Playlists</li>
+          <li>Artists</li>
+          <li>Albums</li>
+          <li>Podcasts & Shows</li>
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
+export function MainMenu() {
+  return (
+    <section>
+      <div aria-describedby="navigation-bar">
+        <ul>
+          <li>
+            Icon
+            <h3>Home</h3>
+          </li>
+          <li>
+            Icon
+            <h3>Home</h3>
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
+}
 export default App
---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,9 @@
+interface Props {
+    
+}
+ 
+const : Dashboard<Props> = () => {
+    return (  );
+}
+ 
+export default ;
